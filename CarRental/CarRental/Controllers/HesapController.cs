83eed4a1144d14d1bcb89eb7b4112e741445be61
Busra Sarikaya <96743085+busrasarikaya98@users.noreply.github.com>
2022-04-15 19:36:48 +0300

    carrental

---
+++ CarRental/CarRental/Controllers/HesapController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Controllers
+{
+    public class HesapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Giris()
+        {
+            return View();
+        }
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] //data güvenliği için token
+        public IActionResult Kayit(string usernamee)
+        {
+            string a = "Ali" + usernamee;
+            return View(a);
+        }
+        public IActionResult SifreSifirla()
+        {
+            return View();
+        }
+        public IActionResult HukumveKosullar()
+        {
+            return View();
+        }
+    }
+}
