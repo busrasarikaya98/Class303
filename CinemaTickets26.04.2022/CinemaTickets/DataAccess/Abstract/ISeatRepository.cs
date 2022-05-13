229a043b58b66a2886a96ef0502dff42c74e08aa
Busra Sarikaya <96743085+busrasarikaya98@users.noreply.github.com>
2022-05-13 21:54:33 +0300

    13.05.2022

--- CinemaTickets26.04.2022/CinemaTickets/DataAccess/Abstract/ISeatRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebApi.Core.DataAccess.Abstract;
-using WebApi.Models;
-
-namespace WebApi.DataAccess.Abstract
-{
-    public interface ISeatRepository:IBaseRepository<Seat> 
-    {
-    }
-}
